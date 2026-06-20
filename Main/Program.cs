ad1149bce3dca50e592404509f0ef18510c4ad9c
Kristonen <walther-christopher@gmx.de>
2026-06-21 01:48:03 +0200

    Add NUNit project to the solution and package (Raylib) to the Main-Projekt

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+RunGame();
+
+
+[STAThread]
+void RunGame()
+{
+    Raylib.InitWindow(1920, 1080, "Shadows are following");
+
+    while (!Raylib.WindowShouldClose()){
+        Raylib.BeginDrawing();
+        Raylib.DrawCircleV(new(500, 500), 50, Color.Purple);
+        Raylib.EndDrawing();
+    }
+}
