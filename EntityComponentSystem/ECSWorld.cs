d3aa86924f662f8a6c13563b414394679b7a766d
Kristonen <walther-christopher@gmx.de>
2026-06-21 02:22:10 +0200

    Add some simple tests and for ECS a property

--- EntityComponentSystem/ECSWorld.cs
+++ EntityComponentSystem/ECSWorld.cs
@@ -1,5 +1,6 @@
 public class ECS
 {
+    public int CurrentEntityId { get => _currentEntityId; }
     public CustomList<ISystem> Systems { get => _systems; set => _systems = value; }
     public Dictionary<int, CustomList<IComponent>> Storage { get => _storage; set => _storage = value; }
     private CustomList<ISystem> _systems;
@@ -9,6 +10,65 @@ public class ECS
     public ECS(){
         _storage = new();
         _systems = new();
-        _currentEntityId = 0;
+        _currentEntityId = 1;
+    }
+
+    public Entity CreateEntity()
+    {
+        Entity entity = new(_currentEntityId);
+        _currentEntityId += 1;
+        return entity;
+    }
+
+    public void AddComponent(int entityId, IComponent component)
+    {
+        if(!_storage.ContainsKey(entityId))
+        {
+            _storage[entityId] = new();
+        }
+        _storage[entityId].Add(component);    
+    }
+
+    public void AddComponent(Entity entity, IComponent component)
+    {
+        AddComponent(entity.Id, component);
+    }
+
+    public bool HasComponent<T>(int entityId) where T : class, IComponent
+    {
+        if(!_storage.ContainsKey(entityId)) return false;
+        for(int i = 0; i < _storage[entityId].Count; i++)
+        {
+            IComponent component = _storage[entityId][i];
+            if(component is T)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool HasComponent<T>(Entity entity) where T : class, IComponent
+    {
+        return HasComponent<T>(entity.Id);
+    }
+
+    public (T component, bool ok)GetComponent<T>(int entityId) where T : class, IComponent
+    {
+        if(!_storage.ContainsKey(entityId)) return (default!, false);
+        for(int i = 0; i < _storage[entityId].Count; i++)
+        {
+            IComponent component = _storage[entityId][i];
+            if(component is T foundedComponent)
+            {
+                return (foundedComponent, true);
+            }
+        }
+        return (default!, false);
+    }
+
+    public (T component, bool ok)GetComponent<T>(Entity entity) where T : class, IComponent
+    {
+        return GetComponent<T>(entity.Id);
     }
 }
---
+++ Tests/EcsRegistryTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tests;
+
+[TestFixture]
+public class EcsRegistryTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    private class TestComponent : IComponent{}
+    private class ComponentValue : IComponent
+    {
+        public int Value { get => _value; set => _value = value; }
+        private int _value;
+    }
+
+    [Test]
+    public void GetComponent_ByEntity_ReturnComponentAndTrue()
+    {
+        ECS ecs = new();
+        var entity = ecs.CreateEntity();
+        var expectedComponent = new TestComponent();
+        ecs.AddComponent(entity, expectedComponent);
+        var (component, ok) = ecs.GetComponent<TestComponent>(entity);
+
+        Assert.That(ok, Is.True);
+        Assert.That(component, Is.Not.Null);
+        Assert.That(component, Is.EqualTo(expectedComponent));
+    }
+
+    [Test]
+    public void GetComponent_ByEntity_ReturnComponentWithRightValue()
+    {
+        ECS ecs = new();
+        ComponentValue expectedComponent = new();
+        int expectedValue = 5;
+        expectedComponent.Value = expectedValue;
+        var entity = ecs.CreateEntity();
+        ecs.AddComponent(entity, expectedComponent);
+        var (component, ok) = ecs.GetComponent<ComponentValue>(entity);
+
+        Assert.That(ok, Is.True);
+        Assert.That(component.Value, Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void GetComponent_WhenComponentNotExist_ReturnDefaultAndFalse()
+    {
+        ECS ecs = new();
+        var entity = ecs.CreateEntity();
+        ecs.AddComponent(entity, new TestComponent());
+        ComponentValue other = new();
+        var (component, ok) = ecs.GetComponent<ComponentValue>(entity);
+        Assert.That(ok, Is.False);
+        Assert.That(component, Is.Null);
+    }
+
+    [Test]
+    public void CheckCurrentEntityId_WhenEntitiesAreCreated_IncreaseCorrectly()
+    {
+        ECS ecs = new();
+        Assert.That(ecs.CurrentEntityId, Is.EqualTo(1));
+
+        ecs.CreateEntity();
+        ecs.CreateEntity();
+        ecs.CreateEntity();
+
+        Assert.That(ecs.CurrentEntityId, Is.EqualTo(4));
+    }
+}
